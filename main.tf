ef0eba3488db98155b7e8109c3d4c880ba53aff8
YamatoKato <yamatotdu@ezweb.ne.jp>
2024-10-19 04:42:47 +0900

    Refactor main.tf format

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 terraform {
+  required_version = "1.3.8"
   backend "s3" {
-    bucket = "yk-aws-ecs-terraform-tfstate"
-    key = "terraform.tfstate"
+    bucket         = "yk-aws-ecs-terraform-tfstate"
+    key            = "terraform.tfstate"
     region         = "ap-northeast-1"
     dynamodb_table = "yk-aws-ecs-terraform-tfstate-locking"
     encrypt        = true
